b3b0e177159de062582c3903179247110aed8f87
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-03-07 09:21:12 +0000

    First iteration

---
+++ ChangeString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please write a phrase:");
+            string phrase = Console.ReadLine();
+            Console.WriteLine("Please insert a character:");
+            char character = char.Parse(Console.ReadLine());
+
+            string modified = phrase.Replace(character, 'x');
+            Console.WriteLine(modified);
+
+
+        }
+    }
+}
